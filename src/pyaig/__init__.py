bdafae1aeaa98a62f617bd8a1e34d73c66f7b850
sterin <sterin@localhost>
2013-05-22 15:48:38 -0700

    simulator

--- src/pyaig/__init__.py
+++ src/pyaig/__init__.py
@@ -1,7 +1,7 @@
 from aig import AIG
 from aigexpr import aigexpr
 
-from aig_io import read_aiger
-from aig_io import write_aiger
-
+from aig_io import read_aiger, write_aiger
 from aig_io import write_cnf
+
+from simulate import read_cex, simulate, print_cex
