a420a2330ef0aa039011414ef9c3a89a43a3d8c9
sterin <github@bsterin.com>
2014-05-07 11:58:39 -0700

    added setup.py for easy installation

---
+++ src/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name='PyAIG',
+    version='1.0',
+    description='Python AIG package',
+    author='Baruch Sterin',
+    author_email='pyaig@bsterin.com',
+    packages=['pyaig'],
+)
+
+
