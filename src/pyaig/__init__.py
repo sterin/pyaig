72c139de7e741f6602d80f539a03a4d8bdccb2c3
Baruch Sterin <github@bsterin.com>
2014-08-20 14:19:01 -0700

    reaarange __init__.py

--- src/pyaig/__init__.py
+++ src/pyaig/__init__.py
@@ -7,7 +7,7 @@ from aig_io import write_cnf
 from simulate import read_cex, simulate, print_cex
 
 import primitives
-from truthtables import truth_tables
-
 import utils
+
+from truthtables import truth_tables
 from aig_to_tt import aig_to_tt_map, aig_to_tt, aig_to_tt_fname
