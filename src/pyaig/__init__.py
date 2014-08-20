56010ea5884dfd896b762d07aea1100051cc9f3b
Baruch Sterin <baruchs@gmail.com>
2014-08-20 14:18:57 -0700

    add aig_to_tt, builds truth tables from AIGs

--- src/pyaig/__init__.py
+++ src/pyaig/__init__.py
@@ -9,3 +9,4 @@ from simulate import read_cex, simulate, print_cex
 from truthtables import truth_tables
 
 import utils
+from aig_to_tt import aig_to_tt_map, aig_to_tt, aig_to_tt_fname
---
+++ src/pyaig/aig_to_tt.py
@@ -0,0 +1,48 @@
+from aig import AIG
+from aig_io import read_aiger, write_aiger
+from truthtables import truth_tables
+
+
+class aig_to_tt_map(AIG.fmap):
+    def __init__(self, fs):
+        super(aig_to_tt_map, self).__init__(fs=fs, negate_if_negated=lambda tt,f: tt.negate_if(AIG.is_negated(f)) )
+
+
+def aig_to_tt(aig):
+
+    m = truth_tables(aig.n_pis())
+    M = aig_to_tt_map( (aig.get_pi_by_id(i), m.var(i)) for i in xrange(aig.n_pis()) )
+
+    for f, n in aig.construction_order_deref():
+
+        if f in M:
+            continue
+
+        assert n.is_and(), 'tt_from_aig: only works on a combinational AIG'
+        M[f] = M[n.get_left()] & M[n.get_right()]
+
+    assert (aig.n_pos()%2) == 0
+    return m, [ (M[aig.get_po_fanin(i*2)], M[aig.get_po_fanin(i*2+1)]) for i in xrange(aig.n_pos()/2) ]
+
+
+def aig_to_tt_fname(fname):
+
+    with open(fname, 'r') as f:
+        aig = read_aiger(f)
+
+    return aig_to_tt(aig)
+
+if __name__=="__main__":
+
+    N = 4
+
+    aig = AIG()
+
+    pis = [ aig.create_pi() for i in xrange(N) ]
+    aig.create_po( aig.conjunction(pis) )
+    aig.create_po( aig.create_and( aig.disjunction(pis), AIG.negate(aig.conjunction(pis)) ) )
+
+    m, res = aig_to_tt(aig)
+
+    for f,r in res:
+        print 'f=', f, ' r=', r
