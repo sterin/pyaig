c40476cb9622fad2d18c358dd243c93837ed5b9c
Baruch Sterin <github@bsterin.com>
2017-11-05 16:29:32 -0800

    added a function aig_cut_to_tt() to build a truth table from a cut in the AIG. refactor a little bit.

--- src/pyaig/aig_to_tt.py
+++ src/pyaig/aig_to_tt.py
@@ -4,11 +4,56 @@ from truthtables import truth_tables
 
 
 class aig_to_tt_map(AIG.fmap):
-    def __init__(self, m, fs):
+    def __init__(self, m, fs=()):
         super(aig_to_tt_map, self).__init__(fs=fs, negate_if_negated=lambda tt,f: tt.negate_if(AIG.is_negated(f)), zero=m.const(0) )
 
 
+def aig_cut_to_tt(m, aig, f, cut):
+    """ Build a truth table for a node 'f' and a cut 'cut'.
+    >>> aig = AIG()
+    >>> f = aig.conjunction([ aig.create_pi() for _ in xrange(6) ])
+    >>> m = truth_tables(4)
+    >>> print aig_cut_to_tt(m, aig, AIG.negate(f), [3, 5, 6, 9, 10, 12])
+    !x0 + x1 + !x2 + !x3
+    """
+
+    M = aig_to_tt_map(m)
+
+    for c in cut[:-m.N]:
+        M[c] = m.const(0)
+
+    for i, c in enumerate(cut[-m.N:]):
+        M[c] = m.var(i)
+
+    def rec(f):
+
+        if f in M:
+            return M[f]
+
+        if AIG.is_negated(f):
+            return ~rec(AIG.negate(f))
+
+        assert not AIG.is_negated(f)
+        assert aig.is_and(f)
+
+        return rec( aig.get_and_left(f) ) & rec( aig.get_and_right(f) )
+
+    return rec(f)
+
+
 def aig_to_tt(aig):
+    """
+    >>> aig = AIG()
+    >>> pis = [ aig.create_pi() for _ in xrange(4) ]
+    >>> po0 = aig.create_po( aig.conjunction(pis) )
+    >>> po1 = aig.create_po( aig.disjunction(pis) )
+    >>> m, tts = aig_to_tt(aig)
+    >>> for f, r in tts:
+    ...     print f, '  -  ' ,r
+    x0&x1&x2&x3   -   x0 + x1 + x2 + x3
+    """
+    assert aig.n_latches() == 0, 'aig_to_tt: combinational AIG expected'
+    assert aig.n_buffers() == 0, 'aig_to_tt: AIG contains unexpected buffers'
 
     m = truth_tables(aig.n_pis())
     M = aig_to_tt_map( m, ((aig.get_pi_by_id(i), m.var(i)) for i in xrange(aig.n_pis())) )
@@ -18,7 +63,6 @@ def aig_to_tt(aig):
         if f in M:
             continue
 
-        assert n.is_and(), 'tt_from_aig: only works on a combinational AIG'
         M[f] = M[n.get_left()] & M[n.get_right()]
 
     assert (aig.n_pos()%2) == 0
@@ -32,17 +76,8 @@ def aig_to_tt_fname(fname):
 
     return aig_to_tt(aig)
 
+
 if __name__=="__main__":
 
-    N = 4
-
-    aig = AIG()
-
-    pis = [ aig.create_pi() for i in xrange(N) ]
-    aig.create_po( aig.conjunction(pis) )
-    aig.create_po( aig.create_and( aig.disjunction(pis), AIG.negate(aig.conjunction(pis)) ) )
-
-    m, res = aig_to_tt(aig)
-
-    for f,r in res:
-        print 'f=', f, ' r=', r
+    import doctest
+    doctest.testmod()
