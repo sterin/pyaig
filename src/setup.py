481647777c7657908f84e0837049c30ff1a0746d
Baruch Sterin <github@bsterin.com>
2017-11-05 21:59:17 -0800

    setup.py: (1) add requirements (2) provide some more information

--- src/setup.py
+++ src/setup.py
@@ -4,11 +4,15 @@ from setuptools import setup
 
 setup(
     name='PyAIG',
-    version='1.0',
-    description='Python AIG package',
+    version='1.0.1',
+    license='MIT',
+    description='A simple Python AIG package',
+    platforms='any',
     author='Baruch Sterin',
     author_email='pyaig@bsterin.com',
+    install_requires=[
+        'click',
+        'future'
+    ],
     packages=['pyaig'],
 )
-
-
