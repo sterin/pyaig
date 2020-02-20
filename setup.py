050a135de2c616da2ad043e9a3f467679d2bdfbd
Baruch Sterin <baruchs@gmail.com>
2020-02-20 10:40:49 +0200

    Prepare to upload to PyPi

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python
 
+import os
 from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name='PyAIG',
-    version='1.0.1',
+    version='1.0.2',
     license='MIT',
-    description='A simple Python AIG package',
-    platforms='any',
-    author='Baruch Sterin',
+    author=u'Baruch Sterin',
     author_email='pyaig@bsterin.com',
+    url='http://github.com/sterin/pyaig',
+    description='A simple Python AIG package',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    platforms='any',
     install_requires=[
         'click',
         'future'
